9bb5e5b118d889443b368bda7719ceca5495bddc
gkhan496 <gguzelkokar.mdbf15@iste.edu.tr>
2023-02-11 02:26:33 +0300

    new tld

--- phishing.py
+++ phishing.py
@@ -11,7 +11,7 @@ stri = ""
 
 tlds = [".com", ".net", ".org", ".xyz", ".biz", ".co", ".ru", ".site",
         ".com.tr", ".cf", ".ga", ".gq", ".fun", ".one", ".online",
-        ".xn--p1ai", ".de", ".fr", ".es", ".store", ".ml", ".tk", ".org.tr"]
+        ".xn--p1ai", ".de", ".fr", ".es", ".store", ".ml", ".tk", ".org.tr",".world"]
 
 for s in itertools.permutations(words, 1):
     for tld in tlds:
